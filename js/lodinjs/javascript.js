c66457454a4ac64075ca64228e5668fd0ff8c5cd
Cyrus <enjoim33@gmail.com>
2023-06-26 14:38:21 -0400

    fizzbuzz

---
+++ js/lodinjs/javascript.js
@@ -0,0 +1,40 @@
+//function favCar(car) {
+//    return car + " is cooler than the rest!"
+//}
+
+//console.log(favCar('Saleen'))
+
+//function showMessage(pls) {
+//   return pls + 'this is a test'
+//}
+
+//console.log(showMessage('Hey, do you know if '))
+
+//function add7(seven) {
+//    return seven + 7
+//}
+
+//console.log(add7(20))
+
+//function multiply(math2) {
+//    return math2 * 2
+//}
+//console.log(multiply(50))
+
+
+
+
+//function capitalize(sentence) {
+//    return sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+//}
+//console.log(capitalize("the EARth lives in THE habITABLE zONE"))
+
+//function lastLetter(rr) {
+//    return rr.slice(-1);
+//}
+//console.log(lastLetter('abc'));
+
+//function firstLetter(test1) {
+//    return test1.charAt(0);
+//}
+//console.log(firstLetter('abc'))
